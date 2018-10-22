a64482ce99dff770fdd38faa8330fc2b39249819
clamascabaleiro <clamascabaleiro@nautilus15.danielcastelao.org>
2018-10-22 10:37:21 +0200

    Ejercicio Terminado

--- Boletin3_1/src/boletin3_1/Boletin3_1.java
+++ Boletin3_1/src/boletin3_1/Boletin3_1.java
@@ -16,13 +16,13 @@ public class Boletin3_1 {
      */
    public static void main(String[] args) {
         // TODO code application logic here
-        Consumo consumo1 = new Consumo();
-        consumo1.setLitros(50f);
-        consumo1.setPgas(1.57f);
-        Consumo consumo2 = new Consumo(451f, 65.7f, 74.3f, 1.57f);
-        System.out.println("consumo medio = " + consumo2.consumoMedio());
-        consumo2.setLitros(56.2f);
-        System.out.println("velocidade media" + consumo2.getVmed());
+        Consumo coche1 = new Consumo();
+        coche1.setLitros(50f);
+        coche1.setPgas(1.57f);
+        Consumo coche2 = new Consumo(451f, 65.7f, 74.3f, 1.57f);
+        System.out.println("consumo medio = " + coche2.consumoMedio());
+        coche2.setLitros(56.2f);
+        System.out.println("velocidade media" + coche2.getVmed());
     }
        
 }
